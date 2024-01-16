772f585c183721bb0aed7ccb6d7eb739389f7ed9
Heng GAO <heng.gao.us@gmail.com>
2024-01-15 19:47:26 -0600

    day-1: use the data table component to render the basic table from the data

--- src/routes/advent/day-1/+page.svelte
+++ src/routes/advent/day-1/+page.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
-    import type { PageData } from './$types';
+	import type { PageData } from "./$types";
+	import DataTable from "$lib/components/ui-extended/data-table.svelte";
 
+    // Received data from the `+page.ts` universal load function
     export let data: PageData;
 </script>
 
-{#each data.elfTallyRecords as record}
-    <div>
-        <span>{record.name}</span>
-        <span>{record.tally}</span>
-    </div>
-{/each}
+<div class="container mx-auto py-10">
+    <!-- Generating the table using the data table component from data-table.svelte -->
+    <!-- Passing the records to the data table component -->
+	<DataTable bind:records={data.elfTallyRecords} />
+</div>
