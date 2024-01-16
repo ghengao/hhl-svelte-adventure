c2c4a229b985a90663cf6598a9c8ae0d99ab87c6
Heng GAO <heng.gao.us@gmail.com>
2024-01-15 18:42:25 -0600

    day-1: add tailwindcss and postcss and shadcn-svelte dependencies

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="text-5xl font-bold underline">Welcome to SvelteKit</h1>
 
 <nav>
 	<a href="/advent/day-1">Day 1</a>
-	<a href="/advent/day-2">Day 1</a>
 </nav>
