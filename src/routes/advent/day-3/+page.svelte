0a81d31b216e8427de2a3731d099c340dd9abe88
Heng GAO <heng.gao.us@gmail.com>
2024-01-22 19:31:14 -0600

    day-3: finish up day-3 challenge

--- src/routes/advent/day-3/+page.svelte
+++ src/routes/advent/day-3/+page.svelte
@@ -9,39 +9,41 @@
 	};
 
 	let weights: Writable<Weights[]> = persistable<Weights[]>("sleighWeights", []);
-	onMount(() => {
-		if ($weights.length === 0) {
-			console.log("fetching weights");
-			fetch("https://advent.sveltesociety.dev/data/2023/day-three.json")
-				.then((res) => res.json())
-				.then((data) => {
-					$weights = data;
-				});
-		}
-	});
 
 	type Trip = {
 		totalWeight: number;
 		weights: Weights[];
 	};
 
-    // trips maintains the state of computed trips from the weights data.
-    // TODO: Need a button to reset the trips data to restore the state.
-    // TODO: Need a way to remove the trip if the trip is empty.
-	// TODO: Add a button to trigger this, otherwise it will be called on every refresh and load the data from original data store.
+	// trips maintains the state of computed trips from the weights data.
 	let trips = persistable<Trip[]>("sleighTrips", []);
 
-	designTrips($weights);
+    async function fetchData() {
+        if ($weights.length === 0) {
+            console.log("fetching weights");
+            let res = await fetch("https://advent.sveltesociety.dev/data/2023/day-three.json")
+            let data = await res.json()
+            $weights = data;
+            designTrips(data);
+        }
+    }
 
-    // Adding trip to the trips store
-	function addTripWeight(idx: number, weight: Weights) {
+    // function to trigger to fetch and reset the data
+    async function reset() {
+        $weights = [];
+        await fetchData();
+    }
+
+	// Adding trip to the trips store
+	function addTripWeight(idx: number, weight: Weights): boolean {
 		let t = $trips;
 		t[idx].weights.push(weight);
 		t[idx].totalWeight += weight.weight;
 		$trips = t;
+		return true;
 	}
 
-    // Removing trip from the trips store
+	// Removing trip from the trips store
 	function removeTripWeight(idx: number, name: string) {
 		let t = $trips;
 		for (let i = 0; i < t[idx].weights.length; i++) {
@@ -77,62 +79,87 @@
 		$trips = designedTrips;
 	}
 
-    // drag start is dynamic variable to indicate if the drag event is started to control the border style of all the card.
-	let dragStart = false;
+	// drag over index is dynamic variable to indicate the index of the card that is being dragged over to control the border style of the card that is currently being dragged over.
+	let dragOverIndex: number | null = null;
 
-    // drag over index is dynamic variable to indicate the index of the card that is being dragged over to control the border style of the card that is currently being dragged over.
-    let dragOverIndex = -1;
+	// store the dragged data into the following store
+	type DraggedData = {
+		idx: number;
+		name: string;
+		weights: Weights;
+	};
+
+	let draggedData: Writable<DraggedData | null> = writable<DraggedData | null>(null);
 </script>
 
-<div class="flex justify-evenly">
-	{#each $trips as trip, idx}
-		<!-- svelte-ignore a11y-no-static-element-interactions -->
-		<div class="flex flex-col card bg-base-100 shadow-xl">
-			<div
-				class="card-body"
-				class:border-2={dragStart}
-				class:border-dashed={dragStart && dragOverIndex !== idx}
-				class:border-accent={dragStart}
-                class:border-opacity-20={dragStart}
-				on:dragover={(ev) => {
-					ev.preventDefault();
-                    dragStart = true;
-                    dragOverIndex = idx;
-				}}
-				on:drop={(ev) => {
-					ev.preventDefault();
-					if (ev.dataTransfer) {
-						let data = JSON.parse(ev.dataTransfer.getData("text/json"));
-						if (data) {
-							removeTripWeight(data.idx, data.name);
-							addTripWeight(idx, { name: data.name, weight: data.weight });
-						}
-					}
-                    dragStart = false;
-                    dragOverIndex = -1;
-				}}
-			>
-				<h5 class="card-title">Trip {idx}, {trip.totalWeight.toFixed(2)}</h5>
-				{#each trip.weights as weight}
-					<!-- svelte-ignore a11y-no-static-element-interactions -->
+{#await fetchData()}
+Loading...
+{:then _ }
+<div class="flex justify-center items-center min-h-screen">
+	<div class="flex flex-col justify-center items-center">
+		<div class="prose">
+			<h1 class="text-center mb-5">Merry Munch-o-Meter</h1>
+		</div>
+		<div class="flex justify-evenly">
+			{#each $trips as trip, idx}
+				<!-- svelte-ignore a11y-no-static-element-interactions -->
+				<div class="flex flex-col card shadow-xl">
 					<div
-						class="join join-vertical lg:join-horizontal"
-						draggable="true"
-						on:dragstart={(ev) => {
-                            dragStart = true;
-							if (ev.dataTransfer) {
-								ev.dataTransfer.setData(
-									"text/json",
-									JSON.stringify({ name: weight.name, weight: weight.weight, idx: idx })
-								);
+						class="card-body"
+						class:border-2={$draggedData !== null}
+						class:border-dashed={$draggedData !== null && dragOverIndex !== idx}
+						class:border-accent={$draggedData !== null}
+						class:opacity-30={$draggedData !== null &&
+							$draggedData.weights.weight + trip.totalWeight > 100}
+						on:dragover={(ev) => {
+							// When drag over, set the drag over index to the current index so that the drag target can be highlighted.
+							// Also make sure that it is droppable if the total weight of the dragged data and the current trip is less than 100.
+							if ($draggedData !== null && $draggedData.weights.weight + trip.totalWeight <= 100) {
+								ev.preventDefault();
+								dragOverIndex = idx;
+							}
+						}}
+						on:drop={(ev) => {
+							// When start the drag, set the dragged data to the current data.
+							ev.preventDefault();
+							if ($draggedData !== null) {
+								addTripWeight(idx, $draggedData.weights);
+								removeTripWeight($draggedData.idx, $draggedData.name);
+								$draggedData = null;
 							}
 						}}
 					>
-						<div class="badge badge-lg badge-accent join-item">{weight.name}</div>
-						<div class="badge badge-lg join-item">{weight.weight}</div>
+						<figure
+							class="card-title center w-full font-extrabold bg-violet-500 text-xl text-stone-100"
+						>
+							Trip {trip.totalWeight.toFixed(2)}
+						</figure>
+						{#each trip.weights as weight}
+							<!-- svelte-ignore a11y-no-static-element-interactions -->
+							<div
+								class="join join-vertical lg:join-horizontal"
+								draggable="true"
+								on:dragstart={(ev) => {
+									$draggedData = { name: weight.name, weights: weight, idx: idx };
+									if (ev.dataTransfer) {
+										ev.dataTransfer.effectAllowed = "move";
+									}
+								}}
+								on:dragend={() => {
+									$draggedData = null;
+									dragOverIndex = null;
+								}}
+							>
+								<div class="badge badge-lg badge-accent join-item">{weight.name}</div>
+								<div class="badge badge-lg join-item">{weight.weight}</div>
+							</div>
+						{/each}
 					</div>
-				{/each}
-			</div>
+				</div>
+			{/each}
 		</div>
-	{/each}
+
+		<button class="btn btn-primary btn-lg mt-5 pl-20 pr-20" on:click={reset}> Reset </button>
+	</div>
 </div>
+{/await}
